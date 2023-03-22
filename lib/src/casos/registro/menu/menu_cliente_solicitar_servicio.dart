04656cdb420e8cd11743fa3829be83d3382ae091
DanteSparda6066 <102558534+DanteSparda6066@users.noreply.github.com>
2023-03-22 09:57:11 -0600

    Segunda pantalla para el cliente
    Por el momento sigue en desarrollo, sera la segunda pantalla que el cliente pueda acceder para solicitar un servicio (se piensa implementar el registro del servicio mediante un formulario y una conexión a base de datos)

---
+++ lib/src/casos/registro/menu/menu_cliente_solicitar_servicio.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+class SegundaVista extends StatelessWidget {
+  const SegundaVista({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(child: Text("Beans Driver"))),
+        body: const Center(child: Text("Esta es la vista para solicitar los servicios",
+        style: TextStyle(fontSize: 20, fontWeight: FontWeight.normal),
+        textAlign: TextAlign.justify),
+        ),
+    );
+  }
+}
