740b0aa602d55272be0341d2d805ce000dfbb619
KFCMan <leonardo.a.t.m.05@gmail.com>
2023-03-25 16:57:34 -0600

    Registro Usuario + Persona correcto

--- lib/src/casos/registro/pantalla_registro.dart
+++ lib/src/casos/registro/pantalla_registro.dart
@@ -1,8 +1,12 @@
 
+import 'dart:developer';
+
 import 'package:beans_driver_flutter/src/casos/registro/form_registro_cuenta_usuario.dart';
 import 'package:beans_driver_flutter/src/casos/registro/form_registro_datos_usuario.dart';
 import 'package:beans_driver_flutter/src/casos/registro/form_registro_rol_usuario.dart';
 import 'package:beans_driver_flutter/src/casos/registro/form_registro_ubi_usuario.dart';
+import 'package:beans_driver_flutter/src/comun/dialogo_alerta.dart';
+import 'package:beans_driver_flutter/src/modelos/persona.dart';
 import 'package:beans_driver_flutter/src/modelos/usuario.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
@@ -21,11 +25,54 @@ class _PantallaRegistroState extends State<PantallaRegistro> {
 
   int _indexActivo = 0;
   final Usuario usuario = Usuario(usuarioID: 0);
+  final Persona persona = Persona(personaID: 0);
 
   void _aumentaIndex() {
     setState(() {
       _indexActivo++;
     });
+  }
+
+  Future<void> registraDatos() async {
+    Map<String, dynamic> resPersona = await persona.insertaEnDB();
+
+    if ( resPersona['stat'] != 200 ) {
+      // return error
+    }
+    
+    persona.personaID = int.parse( resPersona['_']['personaID'] );
+
+    usuario.personaID = persona.personaID;
+
+    Map<String, dynamic> resUsuario = await usuario.insertaEnDB();
+
+    if ( resUsuario['stat'] != 200 ) {
+      // return error
+    }
+
+    usuario.usuarioID = int.parse( resUsuario['_']['usuarioID'] );
+    
+    /*
+    if ( resUsuario['_']['clienteID'] != null ) {
+      usuario.clienteID = int.parse( resUsuario['_']['clienteID'] );
+    }
+    else if ( resUsuario['_']['taxistaID'] != null ) {
+      usuario.taxistaID = int.parse( resUsuario['_']['taxistaID'] );
+    }
+    */
+
+    // Manda que se creó los datos, faltan activarlo
+    return showDialog(
+      context: context,
+      builder: (context) => DialogoAlerta(
+        titulo: "Usuario creado",
+        icono: Icons.person,
+        contenido: "Su usuario ha sido creado.\nSolo requerimos que revise su correo electrónico para activar su cuenta.",
+        acciones: {
+          'OK':() => log("Acción tomada")
+        }
+      ),
+    );
   }
 
   @override
@@ -66,10 +113,10 @@ class _PantallaRegistroState extends State<PantallaRegistro> {
       case 1: return FormRegUbicacionUsuario(
         formKey: widget.llaveFormulario,
         resultado: () {
-          // Pasa los datos a la instancia de usuario
-          // usuario.estadoID = widget.llaveFormulario.currentState?.value['ubi_estado'] ?? 1;
-          // usuario.municipioID = widget.llaveFormulario.currentState?.value['ubi_municipio'] ?? 1;
-          // usuario.localidadID = widget.llaveFormulario.currentState?.value['ubi_localidad'] ?? 1;
+          // Pasa los datos a la instancia de persona
+          persona.estadoID = widget.llaveFormulario.currentState?.value['ubi_estado'] ?? 1;
+          persona.municipioID = widget.llaveFormulario.currentState?.value['ubi_municipio'] ?? 1;
+          persona.localidadID = widget.llaveFormulario.currentState?.value['ubi_localidad'] ?? 1;
           
           _aumentaIndex();
         }
@@ -79,11 +126,11 @@ class _PantallaRegistroState extends State<PantallaRegistro> {
         formKey: widget.llaveFormulario,
         resultado: () {
           // Pasa los datos a la instancia de usuario
-          // usuario.nombre = widget.llaveFormulario.currentState?.value['nombre'] ?? '';
-          // usuario.apePrimero = widget.llaveFormulario.currentState?.value['apellido_1'] ?? '';
-          // usuario.apeSegundo = widget.llaveFormulario.currentState?.value['apellido_2'] ?? '';
-          // usuario.genero = widget.llaveFormulario.currentState?.value['genero'] ?? '';
-          // usuario.numTel = widget.llaveFormulario.currentState?.value['num_tel'] ?? '';
+          persona.nombre = widget.llaveFormulario.currentState?.value['nombre'] ?? '';
+          persona.apePrimero = widget.llaveFormulario.currentState?.value['apellido_1'] ?? '';
+          persona.apeSegundo = widget.llaveFormulario.currentState?.value['apellido_2'] ?? '';
+          persona.genero = widget.llaveFormulario.currentState?.value['genero'] ?? '';
+          persona.numTel = widget.llaveFormulario.currentState?.value['num_tel'] ?? '';
 
           _aumentaIndex();
         }
@@ -96,7 +143,7 @@ class _PantallaRegistroState extends State<PantallaRegistro> {
           usuario.correo = widget.llaveFormulario.currentState?.value['correo'] ?? '';
           usuario.contrasena = widget.llaveFormulario.currentState?.value['psw_1'] ?? '';
 
-          // await usuario.insertaEnDB();
+          await registraDatos();
         }
       );
       
