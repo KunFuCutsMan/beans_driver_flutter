9eaad170de143edb84c7ea0c118c0d97e4620e3a
KFCMan <leonardo.a.t.m.05@gmail.com>
2023-04-02 19:30:50 -0600

    Creación de usuario+Envío de correo de activación

--- lib/src/casos/registro/pantalla_registro.dart
+++ lib/src/casos/registro/pantalla_registro.dart
@@ -36,34 +36,49 @@ class _PantallaRegistroState extends State<PantallaRegistro> {
   }
 
   Future<void> registraDatos() async {
+     // Inserta a la persona
     Map<String, dynamic> resPersona = await persona.insertaEnDB();
 
     if ( resPersona['stat'] != 200 ) {
-      // return error
+      // ignore: use_build_context_synchronously
+      return showDialog(
+        context: context,
+        builder: (context) => DialogoAlerta(
+          titulo: "Error con el servidor",
+          contenido: "Hubo un error con el ingreso de datos personales: ${resPersona['error']}",
+          acciones: {
+            'OK': () => log("Acción tomada")
+          },
+          icono: Icons.error_outline),
+      );
     }
     
+    // Obten la ID de la persona
     persona.personaID = int.parse( resPersona['_']['personaID'] );
-
     usuario.personaID = persona.personaID;
 
+    // E inserta al usuario
     Map<String, dynamic> resUsuario = await usuario.insertaEnDB();
 
     if ( resUsuario['stat'] != 200 ) {
-      // return error
+      // ignore: use_build_context_synchronously
+      return showDialog(
+        context: context,
+        builder: (context) => DialogoAlerta(
+          titulo: "Error con el servidor",
+          contenido: "Hubo un error con el ingreso de la cuenta: ${resUsuario['error']}",
+          acciones: {
+            'OK': () => log("Acción tomada")
+          },
+          icono: Icons.error_outline),
+      );
     }
 
+    // Obten la ID del usuario
     usuario.usuarioID = int.parse( resUsuario['_']['usuarioID'] );
-    
-    /*
-    if ( resUsuario['_']['clienteID'] != null ) {
-      usuario.clienteID = int.parse( resUsuario['_']['clienteID'] );
-    }
-    else if ( resUsuario['_']['taxistaID'] != null ) {
-      usuario.taxistaID = int.parse( resUsuario['_']['taxistaID'] );
-    }
-    */
 
     // Manda que se creó los datos, faltan activarlo
+    // ignore: use_build_context_synchronously
     return showDialog(
       context: context,
       builder: (context) => DialogoAlerta(
@@ -71,10 +86,42 @@ class _PantallaRegistroState extends State<PantallaRegistro> {
         icono: Icons.person,
         contenido: "Su usuario ha sido creado.\nSolo requerimos que revise su correo electrónico para activar su cuenta.",
         acciones: {
-          'OK':() => log("Acción tomada")
+          'Enviar correo': () async => await enviaCorreoAUsuario()
         }
       ),
     );
+  }
+
+  Future<void> enviaCorreoAUsuario() async {
+
+    Map<String, dynamic> resCorreo = await usuario.enviaCorreoActivacion();
+
+    if ( resCorreo['stat'] != 200 ) {
+      // ignore: use_build_context_synchronously
+      return showDialog(
+        context: context,
+        builder: (context) => DialogoAlerta(
+          titulo: "Error con el servidor",
+          contenido: "Hubo un error al enviar su correo de activación: ${ resCorreo['error'] }",
+          acciones: {
+            'OK': () => log("Acción tomada")
+          },
+          icono: Icons.error_outline),
+      );
+    }
+    else {
+      // ignore: use_build_context_synchronously
+      return showDialog(
+      context: context,
+      builder: (context) => DialogoAlerta(
+        titulo: "Correo enviado",
+        contenido: "Su correo de activación ha sido enviado al correo: ${usuario.correo}\n",
+        acciones: {
+          'OK': () => log("Acción tomada")
+        },
+        icono: Icons.email_outlined)
+    );
+    }
   }
 
   @override
@@ -94,8 +141,7 @@ class _PantallaRegistroState extends State<PantallaRegistro> {
 
       child: Padding(
         padding: const EdgeInsets.only(top: 80, bottom: 80, left: 40, right: 40),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        child: ListView(
           children: [
             construyeFormularios(),
             stepperCircular(),
